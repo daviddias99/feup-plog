04f98adb1deb20e1670ec66c84cce53c6310a9e9
luispcunha <luispplmcunha@gmail.com>
2019-10-08 21:25:30 +0100

    Removed unused rules

--- code/squex.pl
+++ code/squex.pl
@@ -26,31 +26,17 @@ display_board([], [SquareLine | Squares]) :-
 display_board([OctagonLine | Octagons], [SquareLine | Squares]) :-
     display_square_line(SquareLine), nl,
     display_upper_octagon_line, nl,
-    write('|  '), display_octagon_line(OctagonLine), nl,
+    display_full_octagon_line(OctagonLine), nl,
     display_lower_octagon_line, nl,
     display_board(Octagons, Squares).
-
-display_line([]).
-display_line([H | T]) :- write(H), display_line(T).
-
-display_octagons([]).
-display_octagons([Line | Rest]) :- 
-    display_upper_octagon_line, nl,
-    write('|  '), display_octagon_line(Line), nl,
-    display_lower_octagon_line, nl, 
-    display_octagons(Rest).
-
-display_octagon_line([]).
-display_octagon_line([H | T]) :- write(H), write('  |  '), display_octagon_line(T). 
 
 display_upper_octagon_line :- write(' /   \\ /   \\ /   \\ /   \\ /   \\ /   \\ /   \\ /   \\ ').
 display_lower_octagon_line :- write(' \\   / \\   / \\   / \\   / \\   / \\   / \\   / \\   /').
 
-display_squares([]).
-display_squares([Line | Rest]) :-
-    display_square_line(Line),
-    nl,
-    display_squares(Rest).
+display_full_octagon_line(OctagonLine) :- write('|  '), display_octagon_line(OctagonLine).
+
+display_octagon_line([]).
+display_octagon_line([H | T]) :- write(H), write('  |  '), display_octagon_line(T). 
 
 display_square_line([]).
 display_square_line([H | T]) :- write(H), write(' --- '), display_square_line(T).
