d6a861d1f60825a053f316cb90f573ddc80436c2
David Silva <up201705373@fe.up.pt>
2019-11-12 16:13:29 +0000

    Tentative adapting of gameover to the new board syntax

--- code/gameover.pl
+++ code/gameover.pl
@@ -2,19 +2,45 @@
 :- [graph].
 
 %
-% gameover(+Board,+Player) - checks if Player as won.
-%
+% gameover(+Board,-Player) - checks if the game is over, if so returns the player that won
+% Oct, Sq, Height, Width, P1T, P2T, Player, NumJogadas
 
 % first, gameover checks if there is a path that unites Player's board edge-rows
-gameover(Board,Player) :- 
-    test_for_path(Board,Player),
-    get_other_player(Player,OtherPlayer),
-    findall(PMove, (move(OtherPlayer,PMove,Board,NextBoard,NumCuts),NumCuts =\= 0,\+test_for_path(NextBoard,Player)),Result),
-    Result = [].
+
+
+gameover([OctagonBoard,SquareBoard,_,_,_,_,NextPlayer,_-CutFlag|[]],Player) :- 
+
+
+    %  If CutFlag is 1, than we the last play was the first play of a double play
+    %  as such, the NextPlayer is the same one that just played, so the player that
+    %  who's win must be checked is NextPlayer.
+    CutFlag =:= 1,
+
+    check_for_win([OctagonBoard,SquareBoard],NextPlayer).
+
+gameover([OctagonBoard,SquareBoard,_,_,_,_,NextPlayer,_-CutFlag|[]],Player) :- 
+
+    %  If CutFlag is 0 than the next player is the opponent of the player that played the previous play,
+    %  as such the player who's win must be checked is the opponent of NextPlayer.
+    CutFlag =:= 0,
+    get_other_player(NextPlayer,Player);
+    check_for_win([OctagonBoard,SquareBoard],Player).
 
 get_other_player(1,2).
 get_other_player(2,1).
 
+check_for_win(Board,Player) :-
+
+    % First condition for winning is if there is a path unitting both colored edges of a player
+    test_for_path(Board,Player),
+    get_other_player(Player,Opponent),
+
+
+    % Second condition for winning is that there is no possible way for the oponnent to destroy said path
+    % Find all moves (PMove) possible by the Opponent such that there is a cut and that causes Player to not have any
+    % available paths. If there are no such moves, Player won.
+    findall(PMove, (move(Opponent,PMove,Board,NextBoard,NumCuts),NumCuts =\= 0,\+test_for_path(NextBoard,Player)),Result),
+    Result = [].
 
 % check if there is a path uniting Player's board edge-rows (search starts at the upper row)
 % get the starters (octagon pieces of Player that are touching it's upper row)
