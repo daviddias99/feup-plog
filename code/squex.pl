b6260bb755e9d2854a9cf01523727af5193a6e79
David Silva <up201705373@fe.up.pt>
2019-11-17 00:15:56 +0000

    Finished comments in squex.pl

--- code/squex.pl
+++ code/squex.pl
@@ -1,36 +1,31 @@
 :- [display], [input], [game_model], [move], [gameover], [bot]. 
 
-init_board([[
-             [0, 0, 0, 1, 0, 0, 2, 0],
-             [0, 0, 0, 1, 2, 2, 0, 2],
-             [2, 2, 2, 2, 1, 2, 2, 1],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0]
-            ],
-            [
-             [0, 1, 1, 1, 1, 1, 1, 1, 0],
-             [2, 0, 0, 0, 0, 0, 2, 2, 2],
-             [2, 0, 0, 0, 2, 2, 2, 2, 2],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [0, 1, 1, 1, 1, 1, 1, 1, 0]
-            ],
-            8,
-            8]
-).
+%   In this file:
+% 
+% - random_move(+GameState,-Move)
+% - greedy_move(+GameState,-BestMove)
+% - value(+GameState,-Value)
+% - value_next(+GameState, -Value)
+% - get_highest_sub_board_value(+OctagonBoard,+Width,+Height,+Player,+Graph,-Value)
 
-play :-
+
+/**
+*   play
+*   
+*   Main predicate that starts the game, displaying the menu screen and menu options.
+*/
+play() :-
     display_main_screen,
     input_menu_option(Option),
     get_players_type(Option, P1Type, P2Type),
     repeat, input_board_size(Height, Width), generate_initial_game_state(Height, Width, P1Type, P2Type, GameState), !,
     game_loop(GameState).
+
+/**
+*   get_players_type(+Option,-P1Type, -P2Type)
+*
+*   Initialize the player type variables. In case that one or both of the types are a bot also get the bot level from the user
+*/
 
 get_players_type(1, 'P', 'P').
 get_players_type(2, 'P', P2Type) :-
@@ -41,21 +36,42 @@ get_players_type(4, P1Type, P2Type) :-
     input_bot_level(P1Type, 1),
     input_bot_level(P2Type, 2).
 
+/**
+*   continue_game(+NewGameState) 
+*
+*   This predicate checks wheter the game is over and if so displays a gameover message. If not proceeds with the gameloop.
+*/
 continue_game(NewGameState) :-
     gameover(NewGameState, Player), !, display_gameover(NewGameState, Player).
 
 continue_game(NewGameState) :-
     game_loop(NewGameState).
 
+/**
+*   game_loop(+GameState)
+*
+*   Main game loop. Cycle that draws the game, fetchs a move from the bot or the userm,applies it, checks for gameover and repeats the process.
+*/
 game_loop(GameState) :-
     display_game(GameState), 
     repeat, get_move(GameState, Move), move(Move, GameState, NewGameState), !,
     continue_game(NewGameState).
 
+/**
+*   get_move(+GameState,+Move)
+*   
+*   Get a move from the user or from the bots.
+*/
 get_move(GameState, Move) :-
     get_game_current_player_type(GameState, Type),
     choose_move(GameState, Type, Move).
 
+/**
+*   choose_move(+GameState,+Move)
+*
+*   Given a Gamestate and it's move-limitations choose_move will prompt the user (if the current player type is a human player) or fetch the next
+*   move from the correct bot.
+*/
 choose_move(GameState, 'P', Move) :-
     get_game_board_size(GameState, Height, Width),
     input_move(Move, Height, Width).
@@ -69,31 +85,3 @@ choose_move(GameState, 2, Move) :-
     greedy_move(GameState, Move),
     write('CHOSEN MOVE: '), write(Move), nl,
     press_enter_to_continue.
-
-
-
-
-
-test :- init_board(OB, SB), display_board(OB, SB, 0, 8, 8), nl,nl, remove_cuttable_squares(OB, SB, 2, NSB), display_board(OB, NSB, 0, 8, 8).
-
-init_board([
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0],
-             [1, 0, 0, 0, 0, 0, 0, 0],
-             [1, 2, 0, 0, 0, 0, 0, 0],
-             [0, 0, 2, 0, 0, 1, 0, 0]
-            ],
-            [
-             [0, 1, 1, 1, 1, 1, 1, 1, 0],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [2, 0, 0, 0, 0, 0, 0, 0, 2],
-             [2, 0, 2, 0, 0, 0, 0, 0, 2],
-             [0, 1, 1, 1, 1, 1, 1, 1, 0]
-            ]).
