5aab27c9b4b6afb760dca5b37ae678fdd72f2b5d
David Silva <up201705373@fe.up.pt>
2019-12-27 10:59:46 +0000

    Initial solving for the problem

--- project_2_construction/code/construction.pl
+++ project_2_construction/code/construction.pl
@@ -28,17 +28,34 @@ obra([150, 10, 10]).
 
 
 dostuff(Vars) :-
+    tell('file.txt'),
+    % Fetch variables
     trabalhadores(T),
     operacoes(Ops),
     precedencias(Prec),
     obra([Custo, Duracao, Bonus]),
+
+    % Init tasks
     length(T, Nworkers),
     initTasks(Ops, Tasks, Nworkers),
-    print(Tasks),
+
+    % Restriction #1
     cumulative(Tasks, [limit(Nworkers)]),
-    initTasksWorkersMatrix(Ops, Tasks, Matrix, W, Nworkers),
+    
+    % Restriction #2, #3 and #4
+    initTasksWorkersMatrix(Ops, Tasks, Matrix, T, Nworkers),
+
+    % Resticton #5
+    transpose(Matrix,TransposedMatrix),
+    imposeNoOverLaps(Tasks,TransposedMatrix),
+
+    % Solution search
     getVars(Tasks, Vars),
-    labeling([], Vars).
+    flattenMatrix(Matrix,FlatMatrix,[]),
+    append(Vars,FlatMatrix,FinalVars),
+    labeling([], FinalVars),
+    write(Tasks), write('\n'),write(Matrix),
+    told.
 
 initTasks([], [], _).
 initTasks([[ID, _Esp, Dbase | _] | RestOps], [task(Oi, Di, Ei, Hi, ID) | RestTasks], Nworkers) :-
@@ -49,25 +66,68 @@ initTasks([[ID, _Esp, Dbase | _] | RestOps], [task(Oi, Di, Ei, Hi, ID) | RestTas
     initTasks(RestOps, RestTasks, Nworkers).
 
 getVars([], []).    
-getVars([task(Oi, Di, Ei, Hi, _) | Tasks], [Oi, Di, Ei, Hi | Vars]) :- getVars(Tasks, Vars). 
+getVars([task(Oi, Di, Ei, Hi, ID) | Tasks], [Oi, Di, Ei, Hi, ID| Vars]) :- getVars(Tasks, Vars). 
 
-
-initTasksWorkersMatrix([], [], [], _).
+initTasksWorkersMatrix([], [], [], _, _).
 initTasksWorkersMatrix([[_ID, Esp, _Dbase, _Custo] | Ops], [task(_Oi, _Di, _Ei, Hi, _ID) | Tasks], [NewRow | Matrix], Workers, Nworkers) :-
+    
+    % Restriction #2
     length(NewRow, Nworkers),
     domain(NewRow, 0, 1),
+    
+    % Restriction #3
     sum(NewRow, #=, Hi),
-    atLeastOneSpecialty(1, NewRow, Esp, Workers),
-    initTasksWorkersMatrix(Ops, Tasks, Workers, Nworkers).
 
-atLeastOneSpecialty(I, NewRow, Specialty, Workers) :-
+    % Restriction #4
+    atLeastOneSpecialty(NewRow, Esp, Workers),
+
+    initTasksWorkersMatrix(Ops, Tasks, Matrix, Workers, Nworkers).
+
+% At least one specialty
+
+atLeastOneSpecialty(NewRow, Specialty, Workers) :-
     specialtyList(Specialty, Workers, SpecialtyList),
     scalar_product(SpecialtyList, NewRow, #>, 0).
+
+% Get specialty List of a worker
 
 specialtyList(_, [], []).
 specialtyList(Specialty, [[_, WorkerSpecialties] | Workers], [B | List]) :-
     scalar_product_reif(WorkerSpecialties, Specialty, #>, 0, B),
     specialtyList(Specialty, Workers, List).
 
+% Impose no overlaps
 
+imposeNoOverLaps(_,[]).
+imposeNoOverLaps(Tasks,[Row|TransposedMatrix]) :-
+    length(Row,Length),
+    StoppingIndex is Length + 1,
+    imposeNoOverlap(1,Tasks,Row,Lines,[],StoppingIndex),
+    disjoint1(Lines),
+    imposeNoOverLaps(Tasks,TransposedMatrix).
 
+% Impose no overlap
+
+imposeNoOverlap(StoppingIndex,_,_,Lines,Lines,StoppingIndex).
+imposeNoOverlap(Index,Tasks,Row,Lines,LinesAcc,StoppingIndex) :-
+
+    element(Index,Row,1),
+    nth1(Index,Tasks,task(Oi, Di, Ei, Hi, ID)),
+    Line = line(Oi,Di),
+    append(LinesAcc,[Line],NewLinesAcc),
+    NewIndex is Index + 1,
+    imposeNoOverlap(NewIndex,Tasks,Row,Lines,NewLinesAcc,StoppingIndex).
+
+imposeNoOverlap(Index,Tasks,Row,Lines,LinesAcc,StoppingIndex) :-
+
+    element(Index,Row,0),
+    NewIndex is Index + 1,
+    imposeNoOverlap(NewIndex,Tasks,Row,Lines,LinesAcc,StoppingIndex).
+
+% Flatten matrix
+
+flattenMatrix([],Result,Result).
+
+flattenMatrix([H|T],Result,Acc) :-
+    append(Acc,H,Acc1),
+    flattenMatrix(T,Result,Acc1).
